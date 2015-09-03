7a620282d6e0d50aeff36797cb55ea0a281a4c1a
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-09-03 17:29:14 -0500

    fix service obj factory

--- client/app/services/auth.service.js
+++ client/app/services/auth.service.js
@@ -1,43 +1,46 @@
-    angular.module('spkr.auth.services', []);
+angular.module('spkr.auth.services', [])
 
-        .factory('AuthService', function ($http, $location, $window) {
+    .factory('AuthService', function ($http, $location, $window) {
 
-        var service = {};
 
-        service.login = login;
-        service.signup = signup;
-        service.isAuth = isAuth;
-        service.signout = signout;
+    var login = function (user) {
+        return $http({
+            method: 'POST',
+            url: '/api/users/login',
+            data: user
+        })
+        .then(function (res) {
+            return res.data.token;
+        })
+    }
 
-        return service;
+    var signup = function (user) {
+        return $http({
+            method: 'POST',
+            url: '/api/users/signup',
+            data: user
+        })
+        .then(function (res) {
+            return res.data.token;
+        })
+    }
 
-        var login = function (user) {
-            return $http({
-                method: 'POST',
-                url: '/api/users/login',
-                data: user
-            })
-            .then(function (res) {
-                return res.data.token;
-            })
-        }
-        var signup = function (user) {
-            return $http({
-                method: 'POST',
-                url: '/api/users/signup',
-                data: user
-            })
-            .then(function (res) {
-                return res.data.token;
-            })
-        }
+    var isAuth = function() {
+        return !!$window.localStorage.getItem('com.spkr');
+    }
 
-        var isAuth = function() {
-            return !!$window.localStorage.getItem('com.spkr');
-        }
+    var signout = function (user) {
+        $window.localStorage.removeItem('com.spkr');
+        $location.path('/login');
+    }
+    
+    var service = {
+        login : login,
+        signup : signup,
+        isAuth : isAuth,
+        signout : signout
+    };
 
-        var signout = function (user) {
-            $window.localStorage.removeItem('com.spkr');
-            $location.path('/login');
-        }
+    return service;
+
 });
