5f56936fa9b0e7f43813c3cfac6b62d5987fb0a7
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-09-04 14:33:40 -0500

    implemented redirect

--- client/app/new-form/newForm.controller.js
+++ client/app/new-form/newForm.controller.js
@@ -1,4 +1,13 @@
 // user creates a new presentation to generate a form
 angular.module('spkr.new-form', [])
-  .controller('NewFormController', function ($scope) {})
+  .controller('NewFormController', function ($scope, $location, $window, Auth) {
+
+        $scope.$watch(Auth.isAuth, function(authed){
+      if (authed) {
+        $location.path('/new-form');
+      } else {
+        $location.path('/login')
+      } 
+    }, true);
+  })
 
--- client/app/previous-forms/previousForms.controller.js
+++ client/app/previous-forms/previousForms.controller.js
@@ -1,3 +1,11 @@
 angular.module('spkr.previous-forms', [])
-  .controller('PrevFormsController', function ($scope, $window, $location) {
+  .controller('PrevFormsController', function ($scope, $window, $location,Auth) {
+
+      $scope.$watch(Auth.isAuth, function(authed){
+    if (authed) {
+      $location.path('/previous-forms');
+    } else {
+      $location.path('/login')
+    } 
+  }, true);
 })
