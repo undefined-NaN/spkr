b2830fd43d7754d50dff4f7c790ed4b9eacd1f09
Ignacio Prado <ignacio@gatogordoweb.com>
2015-09-04 19:23:30 -0500

    Create version 1.0 of CreatePresentation Form

--- client/app/new-form/newForm.controller.js
+++ client/app/new-form/newForm.controller.js
@@ -1,6 +1,8 @@
 // user creates a new presentation to generate a form
 angular.module('spkr.new-form', [])
-  .controller('NewFormController', function ($scope, $location, $window, Auth) {
+  .controller('NewFormController', function ($scope, $location, $window, Auth, 
+  	Pres) {
+  
     $scope.$watch(Auth.isAuth, function(authed){
       if (authed) {
         $location.path('/new-form');
@@ -8,5 +10,16 @@ angular.module('spkr.new-form', [])
         $location.path('/landing')
       } 
     }, true);
+
+    $scope.submit = function(presentation){
+    	
+    	//console.log('in NewFormController#submit');
+    	//console.log(Pres);
+    	
+    	Pres.createPresentation(presentation).then(function(data){
+    		console.log(data);
+    	});
+    };
+
   })
 
