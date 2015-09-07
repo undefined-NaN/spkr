808dd5c43b05d3cce3ba8bac53b02e459a629b8b
Eric <hoang.e.nguyen@gmail.com>
2015-09-07 13:50:15 -0500

    Refactor feedback and presentation controller files

--- server/feedback/feedbackController.js
+++ server/feedback/feedbackController.js
@@ -12,37 +12,33 @@ module.exports = {
   add: function(req, res, next){
     var presentationId = mongoose.Types.ObjectId(req.params.id),
         feedbackId,
-        findPresentation = Q.nbind(Presentation.findOne, Presentation);
+        findPresentation = Q.nbind(Presentation.findOne, Presentation),
+        create = Q.nbind(Feedback.create, Feedback);
 
-    var create = Q.nbind(Feedback.create, Feedback);
+    var newFeedback = {
+      _presentation: presentationId,
+      organization: req.body.organization,
+      clarity: req.body.clarity,
+      volume: req.body.volume,
+      posture: req.body.posture,
+      prepared: req.body.prepared,
+      visualAids: req.body.visualAids,
+      connect: req.body.connect,
+      question: req.body.question,
+      overall: req.body.overall
+    };
 
-    
-    findPresentation({_id: presentationId})
+    create(newFeedback)
+    .then(function(feedback){ 
+      feedbackId = feedback.id
+    })
+    .then(function(){
+      return findPresentation({_id: presentationId})
+    })
     .then(function(presentation){
-      if(!presentation){
-        next(new Error('Cannot find presentation'))
-      }else {
-        var newFeedback = {
-        _presentation: presentationId,
-        organization: req.body.organization,
-        clarity: req.body.clarity,
-        volume: req.body.volume,
-        posture: req.body.posture,
-        prepared: req.body.prepared,
-        visualAids: req.body.visualAids,
-        connect: req.body.connect,
-        question: req.body.question,
-        overall: req.body.overall
-      }
-
-    create(newFeedback).then(function(feedback){
-     feedbackId = feedback.id;
-    })
       presentation.feedbacks.push(feedbackId)
-      presentation.save();
-      console.log(presentation);
-      res.send("Thanks for providing feedback!");
-    }
+      presentation.save()
+      res.json("Thanks for providing feedback!")
     })
-  },
-};
+  }
+}
