efc93e792c36eedc0cb8ef8cd40dd741ea97ff1f
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-09-08 19:12:46 -0500

    basic previous form spec

--- specs/client/prevFormCtrlSpec.js
+++ specs/client/prevFormCtrlSpec.js
@@ -1,1 +1,24 @@
-/* KARMA TEST wip */
+"use strict";
+
+describe('PrevFormsController', function () {
+  var $scope, $rootScope, createController, Auth, $httpBackend;
+
+  beforeEach(module('spkr'));
+  beforeEach(inject(function($injector) {
+
+  $rootScope = $injector.get('$rootScope');
+  $httpBackend = $injector.get('$httpBackend');
+  Auth = $injector.get('Auth');
+  $scope = $rootScope.$new();
+
+  var $controller = $injector.get('$controller');
+
+    createController = function () {
+      return $controller('PrevFormsController', {
+        $scope: $scope,
+        Auth: Auth
+      });
+    };
+  }));
+
+});
