84ff8b0904f5914cfbd6250f75fbd893075ff9da
Vahagn Stepanyan <vahagnst@gmail.com>
2015-09-09 14:15:09 -0500

    seperated criteria and scores inthe feedback collection in the database

--- server/feedback/feedbackModel.js
+++ server/feedback/feedbackModel.js
@@ -5,40 +5,8 @@ var Schema = mongoose.Schema;
 var FeedbackSchema = new mongoose.Schema({
   _presentation: {type: Schema.Types.ObjectId, ref: 'presentation'},
 
-  organization: {
-    type: Number,
-    required: true,
-  },
-  clarity: {
-    type: Number,
-    required: true,
-  },
-  volume: {
-    type: Number,
-    required: true,
-  },
-  posture: {
-    type: Number,
-    required: true,
-  },
-  prepared: {
-    type: Number,
-    required: true,
-  },
-  visualAids: {
-    type: Number,
-    required: true,
-  },
-  connect: {
-    type: Number,
-    required: true,
-  },
-  question: {
-    type: Number,
-    required: true,
-  },
-  overall: {
-    type: Number,
+  scores: {
+    type: Array,
     required: true,
   }
 
