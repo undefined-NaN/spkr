e6beff7ff471cb42238a64030c352e77dc17bda5
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-09-08 19:50:25 -0500

    closes #41

--- specs/client/newFormCtrlSpec.js
+++ specs/client/newFormCtrlSpec.js
@@ -1,21 +1,20 @@
 "use strict";
 
-describe('NewFormController', function () {
+describe('NewFormController', function() {
   var $scope, $rootScope, createController, Auth, Pres, $httpBackend;
 
   beforeEach(module('spkr'));
   beforeEach(inject(function($injector) {
 
-    // mock out our dependencies
     $rootScope = $injector.get('$rootScope');
     $httpBackend = $injector.get('$httpBackend');
     Auth = $injector.get('Auth');
-    Pres = $injector.get('Pres');    
+    Pres = $injector.get('Pres');
     $scope = $rootScope.$new();
 
     var $controller = $injector.get('$controller');
 
-    createController = function () {
+    createController = function() {
       return $controller('NewFormController', {
         $scope: $scope,
         Auth: Auth,
@@ -24,7 +23,7 @@ describe('NewFormController', function () {
     };
   }));
 
-  it('should have a submit method on the $scope', function () {
+  it('should have a submit method on the $scope', function() {
     createController();
     expect($scope.submit).to.be.a('function');
   });
