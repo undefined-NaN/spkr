0bc9a6bbc847f10735ec1faab221dcfa90ce7ea4
Vahagn Stepanyan <vahagnst@gmail.com>
2015-09-14 17:10:54 -0500

    refactored d3 into factory functions and commented code

--- client/app/homepage/homepage.controller.js
+++ client/app/homepage/homepage.controller.js
@@ -1,5 +1,5 @@
 angular.module('spkr.homepage', [])
-  .controller('HomepageController', function ($scope, $window, $location, Auth) {
+  .controller('HomepageController', function ($scope, $window, $location, Auth, Vis) {
 
     $scope.$watch(Auth.isAuth, function(authed){
       if (authed) {
@@ -8,25 +8,20 @@ angular.module('spkr.homepage', [])
         $location.path('/')
       } 
     }, true);
-
-    $scope.$on('$viewContentLoaded', function(){
-      //view loaded do some stuff.
-      $location.replace(); //clear last history route
-    });
-
+    
+    //get all the user's data
     Auth.getAllData()
-
     .then(function(data){
-
+      //the first element of the array conatains the username, 
+      //the remaining elements are presentations
       $scope.user = data[0].username;
-
-      if (data.length > 1) {
-
+      if (data.length > 1) { //if the user has any presentations
+        //get the criteria list from the first presentation (since they are all the same)
         var criteria = data[1].criteria;
-
+        //create an array of objects for each presentation which includes the date, title, and average score for each criteria
         var scoresData = [];
         for (var i = 1; i < data.length; i++){
-          if (data[i].feedbacks.length > 0) {
+          if (data[i].feedbacks.length > 0) { //only add the presentations with feedbacks
             var sums = [];
             for (var j = 0; j < criteria.length; j++){
               sums.push(0);
@@ -40,179 +35,29 @@ angular.module('spkr.homepage', [])
             scoresData.push({date: data[i].date.slice(0,10), title: data[i].title, scores: avgs});
           }
         }
-
-        if (scoresData.length === 0) {
+        //sort the scoresData by date
+        scoresData.sort(function(a, b) {
+          if (a.date > b.date) {
+            return 1;
+          }
+          if (a.date < b.date) {
+            return -1;
+          }
+          return 0;
+        });
+        if (scoresData.length === 0) { //if there are no presentations with feedbacks
           $("#fallbackMessage").append(
           "<h2>Oh no!</h2><p>It looks like you haven't recieved any feedback yet." +
           "  Make sure to give out your <a href='/#/presentations'>feedback form URL</a> to start recieving feedback!</p>")
         } else {
-
-          scoresData.sort(function(a, b) {
-            if (a.date > b.date) {
-              return 1;
-            }
-            if (a.date < b.date) {
-              return -1;
-            }
-            return 0;
-          });
-
-          var barColor = 'steelblue';
-          var segColor = ["red","green","orange","grey","purple","cyan","lightgreen","pink","maroon"];
-
-          function dateChart(data){
-
-            $('#skill').text('scores by criteria for all presentations');
-
-            var DC = {};
-            var DCDim = {t: 15, r: 0, b: 30, l: 0};
-            DCDim.w = 1000 - DCDim.l - DCDim.r;
-            DCDim.h = 200 - DCDim.t - DCDim.b;
-                
-            var DCsvg = d3.select('#dateChart').append("svg")
-                          .attr("width", DCDim.w + DCDim.l + DCDim.r)
-                          .attr("height", DCDim.h + DCDim.t + DCDim.b).append("g")
-                          .attr("transform", "translate(" + DCDim.l + "," + DCDim.t + ")");
-
-            var x = d3.scale.ordinal().rangeRoundBands([0, DCDim.w], 0.1, 0)
-                      .domain(data.map(function(d) { return d[0]; }));
-
-            DCsvg.append("g").attr("class", "x axis")
-                 .attr("transform", "translate(0," + DCDim.h + ")")
-                 .call(d3.svg.axis().scale(x).orient("bottom"));
-
-            var y = d3.scale.linear().range([DCDim.h, 0])
-                      .domain([0, d3.max(data, function(d) { return d[1]; })]);
-
-            var bars = DCsvg.selectAll(".bar").data(data).enter()
-                            .append("g").attr("class", "bar");
-            
-            bars.append("rect")
-                .attr("x", function(d) { return x(d[0]); })
-                .attr("y", function(d) { return y(d[1]); })
-                .attr("width", x.rangeBand())
-                .attr("height", function(d) { return DCDim.h - y(d[1]); })
-                .attr('fill',barColor)
-                .on("mouseover",mouseover)
-                .on("mouseout",mouseout);
-                
-            bars.append("text").text(function(d){ return d3.format(",")(d[1])})
-                .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
-                .attr("y", function(d) { return y(d[1])-5; })
-                .attr("text-anchor", "middle");
-            
-            function mouseover(d){  
-              SC.update(scoresData.filter(function(s){return s.date === d[0];})[0].scores.map(function(s,i){return [criteria[i],s];}));
-              $('#skill').text('scores by criteria for "' + d[2] + '"');
-            }
-            
-            function mouseout(d){   
-              SC.update(dateAverage);
-              $('#skill').text('scores by criteria for all presentations');
-            }
-            
-            DC.update = function(data, color){
-
-              y.domain([0, d3.max(data, function(d) { return d[1]; })]);
-
-              var bars = DCsvg.selectAll(".bar").data(data);
-              
-              bars.select("rect").transition().duration(500)
-                  .attr("y", function(d) { return y(d[1]); })
-                  .attr("height", function(d) { return DCDim.h - y(d[1]); })
-                  .attr("fill", color);
-
-              bars.select("text").transition().duration(500)
-                  .text(function(d){ return d3.format(",")(d[1])})
-                  .attr("y", function(d) { return y(d[1])-5; });     
-            }  
-
-            return DC;
-          }
-
-          function skillChart(data){
-
-            $('#date').text('Total score for each presentation');
-
-            var SC = {};
-            var SCDim = {t: 15, r: 0, b: 30, l: 0};
-            SCDim.w = 1000 - SCDim.l - SCDim.r;
-            SCDim.h = 200 - SCDim.t - SCDim.b;
-                
-            var DCsvg = d3.select('#skillChart').append("svg")
-                          .attr("width", SCDim.w + SCDim.l + SCDim.r)
-                          .attr("height", SCDim.h + SCDim.t + SCDim.b).append("g")
-                          .attr("transform", "translate(" + SCDim.l + "," + SCDim.t + ")");
-
-            var x = d3.scale.ordinal().rangeRoundBands([0, SCDim.w], 0.1, 0)
-                      .domain(data.map(function(d) { return d[0]; }));
-
-            DCsvg.append("g").attr("class", "x axis")
-                 .attr("transform", "translate(0," + SCDim.h + ")")
-                 .call(d3.svg.axis().scale(x).orient("bottom"));
-
-            var y = d3.scale.linear().range([SCDim.h, 0])
-                    .domain([0, d3.max(data, function(d) { return d[1]; })]);
-
-            var bars = DCsvg.selectAll(".bar").data(data).enter()
-                    .append("g").attr("class", "bar");
-            
-            bars.append("rect")
-                .attr("x", function(d) { return x(d[0]); })
-                .attr("y", function(d) { return y(d[1]); })
-                .attr("width", x.rangeBand())
-                .attr("height", function(d) { return SCDim.h - y(d[1]); })
-                .attr("fill", function(d) { return segColor[criteria.indexOf(d[0])]; })
-                .on("mouseover",mouseover)
-                .on("mouseout",mouseout);
-
-            bars.append("text").text(function(d){ return d3.format(",")(d[1])})
-                .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
-                .attr("y", function(d) { return y(d[1])-5; })
-                .attr("text-anchor", "middle");
-            
-            function mouseover(d){ 
-              DC.update(scoresData.map(function(s){return [s.date,s.scores[criteria.indexOf(d[0])],s.title];}),segColor[criteria.indexOf(d[0])]);
-              $('#date').text(d[0] + ' score for each presentation');     
-            }
-            
-            function mouseout(d){   
-              DC.update(skillsAverage,barColor);
-              $('#date').text('Total score for each presentation');
-            }
-            
-            SC.update = function(data){
-
-              y.domain([0, d3.max(data, function(d) { return d[1]; })]);
-              
-              var bars = DCsvg.selectAll(".bar").data(data);
-              
-              bars.select("rect").transition().duration(500)
-                  .attr("y", function(d) {return y(d[1]); })
-                  .attr("height", function(d) { return SCDim.h - y(d[1]); });
-
-              bars.select("text").transition().duration(500)
-                  .text(function(d){ return d3.format(",")(d[1])})
-                  .attr("y", function(d) {return y(d[1])-5; });            
-            }        
-
-            return SC;
-          }
-
-          var dateAverage = criteria.map(function(d,i){return [d,Math.round(d3.mean(scoresData.map(function(t){return t.scores[i];})))];});    
-          var skillsAverage = scoresData.map(function(d){return [d.date,Math.round(d3.mean(d.scores)),d.title]});
-          var DC = dateChart(skillsAverage);
-          var SC = skillChart(dateAverage);    
+          //call the homepageGraph factory function (this is where d3 happens)
+          Vis.homepageGraph(criteria, scoresData);
         }
-      
-      } else {
-        $("#fallbackMessage").append(
-          "<h2>Oh no!</h2><p>It looks like you haven't made any presentations yet.  <a href='/#/presentations'>Create</a> your first presentation to start recieving feedback!</p>")
+      } else { //if the user doesn't have any presentations
+        $("#fallbackMessage").append("<h2>Oh no!</h2><p>It looks like you haven't made any presentations yet.  <a href='/#/presentations'>Create</a> your first presentation to start recieving feedback!</p>")
       }
     })
-
     .catch(function(error){
       console.err(error)
     })
-
   });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -109,3 +109,295 @@ angular.module('spkr.services', [])
       getData: getData
     };
 })
+
+.factory('Vis', function ($http, $location, $window) {
+
+  var homepageGraph = function(criteria, scoresData) {
+    
+    var dateColor = 'steelblue';
+    var skillColor = ["red","green","orange","grey","purple","cyan","lightgreen","pink","maroon"];
+
+    //function to handle the date bar chart
+    function dateChart(data){
+      
+      //add title
+      $('#skill').text('scores by criteria for all presentations');
+
+      var DC = {};
+      var DCDim = {t: 15, r: 0, b: 30, l: 0};
+      DCDim.w = 1000 - DCDim.l - DCDim.r;
+      DCDim.h = 200 - DCDim.t - DCDim.b;
+      
+      //create svg for histogram
+      var DCsvg = d3.select('#dateChart').append("svg")
+                    .attr("width", DCDim.w + DCDim.l + DCDim.r)
+                    .attr("height", DCDim.h + DCDim.t + DCDim.b).append("g")
+                    .attr("transform", "translate(" + DCDim.l + "," + DCDim.t + ")");
+
+      // create function for x-axis mapping
+      var x = d3.scale.ordinal().rangeRoundBands([0, DCDim.w], 0.1, 0)
+                .domain(data.map(function(d) { return d[0]; }));
+
+      // Add x-axis to the histogram svg
+      DCsvg.append("g").attr("class", "x axis")
+           .attr("transform", "translate(0," + DCDim.h + ")")
+           .call(d3.svg.axis().scale(x).orient("bottom"));
+      
+      // Create function for y-axis map
+      var y = d3.scale.linear().range([DCDim.h, 0])
+                .domain([0, d3.max(data, function(d) { return d[1]; })]);
+      
+      // Create bars for histogram to contain rectangles and freq labels
+      var bars = DCsvg.selectAll(".bar").data(data).enter()
+                      .append("g").attr("class", "bar");
+      
+      //create the rectangles
+      bars.append("rect")
+          .attr("x", function(d) { return x(d[0]); })
+          .attr("y", function(d) { return y(d[1]); })
+          .attr("width", x.rangeBand())
+          .attr("height", function(d) { return DCDim.h - y(d[1]); })
+          .attr('fill',dateColor)
+          .on("mouseover",mouseover) // mouseover is defined below
+          .on("mouseout",mouseout); // mouseout is defined below
+          
+      //Create the frequency labels above the rectangles
+      bars.append("text").text(function(d){ return d3.format(",")(d[1])})
+          .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
+          .attr("y", function(d) { return y(d[1])-5; })
+          .attr("text-anchor", "middle");
+      
+      //utility function to be called on mouseover
+      function mouseover(d){ 
+        //filter for selected criteria and update the skill chart
+        SC.update(scoresData.filter(function(s){return s.date === d[0];})[0].scores.map(function(s,i){return [criteria[i],s];}));
+        //update title
+        $('#skill').text('scores by criteria for "' + d[2] + '"');
+      }
+      
+      //utility function to be called on mouseout
+      function mouseout(d){
+        //reset skill chart to original   
+        SC.update(dateAverage);
+        //reset title
+        $('#skill').text('scores by criteria for all presentations');
+      }
+      
+      //create function to update the date chart. This will be used by the skill chart
+      DC.update = function(data, color){
+        // update the domain of the y-axis map to reflect change in magnitured
+        y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+        // Attach the new data to the bars
+        var bars = DCsvg.selectAll(".bar").data(data);
+        // transition the height and color of rectangles
+        bars.select("rect").transition().duration(500)
+            .attr("y", function(d) { return y(d[1]); })
+            .attr("height", function(d) { return DCDim.h - y(d[1]); })
+            .attr("fill", color);
+        // transition the frequency labels location and change value
+        bars.select("text").transition().duration(500)
+            .text(function(d){ return d3.format(",")(d[1])})
+            .attr("y", function(d) { return y(d[1])-5; });     
+      }  
+      return DC;
+    }
+    
+    //function to handle the skill bar chart
+    function skillChart(data){
+
+      //add title
+      $('#date').text('Total score for each presentation');
+
+      var SC = {};
+      var SCDim = {t: 15, r: 0, b: 30, l: 0};
+      SCDim.w = 1000 - SCDim.l - SCDim.r;
+      SCDim.h = 200 - SCDim.t - SCDim.b;
+      
+      //create svg for histogram
+      var DCsvg = d3.select('#skillChart').append("svg")
+                    .attr("width", SCDim.w + SCDim.l + SCDim.r)
+                    .attr("height", SCDim.h + SCDim.t + SCDim.b).append("g")
+                    .attr("transform", "translate(" + SCDim.l + "," + SCDim.t + ")");
+      
+      // create function for x-axis mapping
+      var x = d3.scale.ordinal().rangeRoundBands([0, SCDim.w], 0.1, 0)
+                .domain(data.map(function(d) { return d[0]; }));
+      
+      // Add x-axis to the histogram svg
+      DCsvg.append("g").attr("class", "x axis")
+           .attr("transform", "translate(0," + SCDim.h + ")")
+           .call(d3.svg.axis().scale(x).orient("bottom"));
+      
+      // Create function for y-axis map
+      var y = d3.scale.linear().range([SCDim.h, 0])
+              .domain([0, d3.max(data, function(d) { return d[1]; })]);
+      
+      // Create bars for histogram to contain rectangles and freq labels
+      var bars = DCsvg.selectAll(".bar").data(data).enter()
+              .append("g").attr("class", "bar");
+      
+      //create the rectangles
+      bars.append("rect")
+          .attr("x", function(d) { return x(d[0]); })
+          .attr("y", function(d) { return y(d[1]); })
+          .attr("width", x.rangeBand())
+          .attr("height", function(d) { return SCDim.h - y(d[1]); })
+          .attr("fill", function(d) { return skillColor[criteria.indexOf(d[0])]; })
+          .on("mouseover",mouseover)
+          .on("mouseout",mouseout);
+      
+      //Create the frequency labels above the rectangles
+      bars.append("text").text(function(d){ return d3.format(",")(d[1])})
+          .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
+          .attr("y", function(d) { return y(d[1])-5; })
+          .attr("text-anchor", "middle");
+      
+      //utility function to be called on mouseover
+      function mouseover(d){ 
+        //filter for selected presentation and update the date chart
+        DC.update(scoresData.map(function(s){return [s.date,s.scores[criteria.indexOf(d[0])],s.title];}),skillColor[criteria.indexOf(d[0])]);
+        //update title
+        $('#date').text(d[0] + ' score for each presentation');     
+      }
+      
+      //utility function to be called on mouseout
+      function mouseout(d){   
+        //reset skill chart to original   
+        DC.update(skillsAverage,dateColor);
+        //reset title
+        $('#date').text('Total score for each presentation');
+      }
+      
+      //create function to update the skill chart. This will be used by the date chart
+      SC.update = function(data){
+        // update the domain of the y-axis map to reflect change in magnitured
+        y.domain([0, d3.max(data, function(d) { return d[1]; })]);
+        // Attach the new data to the bars
+        var bars = DCsvg.selectAll(".bar").data(data);
+        // transition the height and color of rectangles
+        bars.select("rect").transition().duration(500)
+            .attr("y", function(d) {return y(d[1]); })
+            .attr("height", function(d) { return SCDim.h - y(d[1]); });
+        // transition the frequency labels location and change value
+        bars.select("text").transition().duration(500)
+            .text(function(d){ return d3.format(",")(d[1])})
+            .attr("y", function(d) {return y(d[1])-5; });            
+      }        
+      return SC;
+    }
+    
+    //create an array of arrays with the criteria and average score for each criteria
+    var dateAverage = criteria.map(function(d,i){return [d,Math.round(d3.mean(scoresData.map(function(t){return t.scores[i];})))];});    
+    //create an array of arrays with the date, title, and average score for each presentation
+    var skillsAverage = scoresData.map(function(d){return [d.date,Math.round(d3.mean(d.scores)),d.title]});
+    //create the date chart
+    var DC = dateChart(skillsAverage);
+    //create the skill chart
+    var SC = skillChart(dateAverage);    
+  }
+
+  var presentationGraph = function(criteria, distData) {
+    
+    //distribution functions for transforming the data
+    function getPoints(_,i){ return _.map(function(d,j){ return {x:j, y:d[i]};}); }
+    function getPointsZero(_,i,k){ return _.map(function(d,j){ return {x:j, y:(i===k ? d[i] : 0 )};}); }
+      
+    var width=800, height=200, margin=20;
+    var colors = ["red","green","orange","grey","purple","cyan","lightgreen","pink","maroon"]
+
+    //create distribution graph
+    function draw(){
+      var maxT = d3.max(distData.map(function(d){ return d3.sum(d); }));
+      
+      function tW(d){ return x(d*(distData.length - 1)/50); }
+      
+      var svg =d3.select("#distChart").select(".dist");
+      
+      //x and y axis maps
+      var x = d3.scale.linear().domain([0, distData.length - 1]).range([0, width]);
+      var y = d3.scale.linear().domain([0, maxT]).range([height, 0]);
+      
+      //graph labels
+      function getHLabel(d,i){ return i*10; } 
+      
+      // add horizontal axis labels
+      svg.append("g").attr("class","hlabels")
+        .selectAll("text").data(d3.range(100).filter(function(d){ return d%5===0})).enter().append("text")
+        .text(getHLabel).attr("x",function(d,i){ return tW(d)-5;}).attr("y",height+14); 
+      
+      var area = d3.svg.area().x(function(d) { return x(d.x); })
+        .y0(function(d) { return y(d.y0); })
+        .y1(function(d) { return y(d.y0 + d.y); })
+        .interpolate("basis");
+
+      var layers = d3.layout.stack().offset("zero")(criteria.map(function(d,i){ return getPoints(distData, i);}));
+      
+      svg.selectAll("path").data(layers).enter().append("path").attr("d", area)
+        .style("fill", function(d,i) { return colors[i]; })
+        .style("stroke", function(d,i) { return colors[i]; });
+    }
+    
+    //utility function for mouseover legend
+    function transitionIn(p){
+      var maxT = d3.max(distData.map(function(d){ return d3.sum(d); }));
+      var max  = d3.max(distData.map(function(d){ return d[p]; }));
+      
+      var x = d3.scale.linear().domain([0, distData.length - 1]).range([0, width]);
+      var y = d3.scale.linear().domain([0, max]).range([height, 0]);
+
+      var area = d3.svg.area().x(function(d) { return x(d.x); })
+        .y0(function(d) { return y(d.y0); })
+        .y1(function(d) { return y(d.y0 + d.y); })
+        .interpolate("basis");
+
+      var layers = d3.layout.stack().offset("zero")(criteria.map(function(d,i){ return getPointsZero(distData, i, p);}));
+      var svg = d3.select("#distChart").select(".dist");
+
+      svg.selectAll("path").data(layers).transition().duration(500).attr("d", area);
+    }
+
+    //utility function for mouseout legend  
+    function transitionOut(){
+      var maxT = d3.max(distData.map(function(d){ return d3.sum(d); }));
+
+      var x = d3.scale.linear().domain([0, distData.length - 1]).range([0, width]);
+      var y = d3.scale.linear().domain([0, maxT]).range([height, 0]);
+
+      var area = d3.svg.area().x(function(d) { return x(d.x); })
+        .y0(function(d) { return y(d.y0); })
+        .y1(function(d) { return y(d.y0 + d.y); })
+        .interpolate("basis");
+      var layers = d3.layout.stack().offset("zero")(criteria.map(function(d,i){ return getPoints(distData, i);}));
+      
+      var svg = d3.select("#distChart").select(".dist");   
+      svg.selectAll("path").data(layers).transition().duration(500).attr("d", area);           
+    }
+
+    function mouseoverLegend(_,p){  
+      transitionIn(p);
+    }
+ 
+    function mouseoutLegend(){  
+      transitionOut();
+    }
+
+    d3.select("#distChart").append("svg").attr("width",width+2*margin).attr("height",height+2*margin)
+      .append("g").attr("transform","translate("+margin+","+margin+")").attr("class","dist");
+
+    draw();     
+    
+    //create legend
+    var legRow = d3.select("#distChart").append("div").attr("class","legend")
+      .append("table").selectAll("tr").data(criteria).enter().append("tr").append("td");
+    legRow.append("div").style("background",function(d,i){ return colors[i];})
+      .on("mouseover",mouseoverLegend).on("mouseout",mouseoutLegend).style("cursor","pointer");
+    legRow.append("span").text(function(d){ return d;})
+      .on("mouseover",mouseoverLegend).on("mouseout",mouseoutLegend).style("cursor","pointer"); 
+
+  }
+  return {
+    homepageGraph: homepageGraph,
+    presentationGraph: presentationGraph
+  };
+
+})
