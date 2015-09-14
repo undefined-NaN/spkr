de2a4b62e893aa3e2488def0f852b34c4c042f9b
Eric <hoang.e.nguyen@gmail.com>
2015-09-14 12:56:09 -0500

    Update server file for deployment

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var app = express();
 
 // configure our server with all the middleware and and routing
 require('./config/middleware.js')(app, express);
-mongoose.connect('mongodb://localhost/spkr');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/spkr');
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
 
