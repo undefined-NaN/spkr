766883edcad71e9d53a64899cd8f76e6a8a2af8d
Jeff Berman <jeffuberman@gmail.com>
2015-09-16 15:28:41 -0500

    make textarea responsive, auto enlarging and gave style

--- client/app/feedback-form/feedbackForm.controller.js
+++ client/app/feedback-form/feedbackForm.controller.js
@@ -51,6 +51,16 @@ angular.module('spkr.feedback-form', ['ngRoute'])
       })
     }
 
+    $scope.autoExpand = function(e) {
+        var element = typeof e === 'object' ? e.target : document.getElementById(e);
+        var scrollHeight = element.scrollHeight -60; // replace 60 by the sum of padding-top and padding-bottom
+        element.style.height =  scrollHeight + "px";    
+    };
+  
+    function expand() {
+      $scope.autoExpand('TextArea');
+    }
+
     $scope.getData();
   });
 
