953c02a17a5704e83bbcddf0c230c75146fbf14d
Jeff Berman <jeffuberman@gmail.com>
2015-09-17 15:44:13 -0500

    correct formatting

--- client/app/previous-pres/previousPres.controller.js
+++ client/app/previous-pres/previousPres.controller.js
@@ -12,10 +12,11 @@ var app = angular.module('spkr.previous-pres', ['ngRoute'])
         $scope.class = "glyphicon-plus-sign";
     
     $scope.changeClass = function(){
-        if ($scope.class === "glyphicon-plus-sign")
-            $scope.class = "glyphicon-minus-sign";
-         else
-            $scope.class = "glyphicon-plus-sign";
+      if ($scope.class === "glyphicon-plus-sign"){
+        $scope.class = "glyphicon-minus-sign";  
+      } else {
+        $scope.class = "glyphicon-plus-sign";
+      }
     };
 
     //get the data for this presentation
@@ -60,50 +61,50 @@ var app = angular.module('spkr.previous-pres', ['ngRoute'])
     .catch(function(error){
       $location.path('/data-profile')
     })
-
   });
 
   app.directive('slideable', function () {
     return {
         restrict: 'C',
         compile: function (element, attr) {
-            // wrap tag
-            var contents = element.html();
-            element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
+          // wrap tag
+          var contents = element.html();
+          element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
 
-            return function postLink(scope, element, attrs) {
-                // default properties
-                attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
-                attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
-                element.css({
-                    'overflow': 'hidden',
-                        'height': '0px',
-                        'transitionProperty': 'height',
-                        'transitionDuration': attrs.duration,
-                        'transitionTimingFunction': attrs.easing
-                });
-            };
+          return function postLink(scope, element, attrs) {
+            // default properties
+            attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
+            attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
+            element.css({
+              'overflow': 'hidden',
+              'height': '0px',
+              'transitionProperty': 'height',
+              'transitionDuration': attrs.duration,
+              'transitionTimingFunction': attrs.easing
+            });
+          };
         }
     };
-})
-    .directive('slideToggle', function () {
+  })
+  .directive('slideToggle', function () {
     return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            var target = document.querySelector(attrs.slideToggle);
-            attrs.expanded = false;
-            element.bind('click', function () {
-                var content = target.querySelector('.slideable_content');
-                if (!attrs.expanded) {
-                    content.style.border = '1px solid rgba(0,0,0,0)';
-                    var y = content.clientHeight;
-                    content.style.border = 0;
-                    target.style.height = y + 'px';
-                } else {
-                    target.style.height = '0px';
-                }
-                attrs.expanded = !attrs.expanded;
-            });
-        }
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var target = document.querySelector(attrs.slideToggle);
+        attrs.expanded = false;
+        element.bind('click', function () {
+          var content = target.querySelector('.slideable_content');
+          if (!attrs.expanded) {
+            content.style.border = '1px solid rgba(0,0,0,0)';
+            var y = content.clientHeight;
+            content.style.border = 0;
+            target.style.height = y + 'px';
+          } else {
+            target.style.height = '0px';
+          }
+          attrs.expanded = !attrs.expanded;
+        });
+      }
     }
-});
+  });
+  
